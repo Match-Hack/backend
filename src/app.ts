b1326b5476d26fe3ecab8ed05661aae7545265b6
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-09-22 21:45:59 +0200

    set up the repo and app.ts

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, {
+    Express
+} from 'express';
+import cors from 'cors';
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const dbURL = process.env.DB_URL ?? "";
+mongoose.connect(dbURL);
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+const app: Express = express();
+
+app.use(cors(
+  {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Referer'],
+  }
+))
+
+export default app;
