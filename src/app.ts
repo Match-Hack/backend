26df215b54042ac2d209a9f2d27a6c6934b67bdb
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-09-23 04:32:39 +0200

    add route to get common POAP between two lens profile

--- src/app.ts
+++ src/app.ts
@@ -6,22 +6,28 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const dbURL = process.env.DB_URL ?? "";
+/*const dbURL = process.env.DB_URL ?? "";
 mongoose.connect(dbURL);
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+db.once('open', () => console.log('Connected to Database'));*/
 
 const app: Express = express();
 
-app.use(cors(
+/*app.use(cors(
   {
     origin: ['http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Referer'],
   }
-))
+))*/
+
+import poapRouter from './routes/POAP';
+import newUserRouter from './routes/newUser';
+
+app.use("/poap",poapRouter);
+app.use(newUserRouter);
 
 export default app;
---
+++ src/routes/POAP.ts
@@ -0,0 +1,58 @@
+import { gql } from 'graphql-request';
+import { init, fetchQuery } from "@airstack/node";
+import express, {
+    Request,
+    Response,
+    Router,
+    json
+} from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY ?? "";
+init(AIRSTACK_API_KEY);
+
+const router = Router();
+router.use(json());
+
+const query = gql`
+query CommonPOAPs($lens1: Identity!, $lens2: Identity!) {
+    user1: Wallet(input: {identity: $lens1, blockchain: ethereum}) {
+      poaps(input: {blockchain: ALL}) {
+        eventId
+      }
+    }
+    user2: Wallet(input: {identity: $lens2, blockchain: ethereum}) {
+      poaps(input: {blockchain: ALL}) {
+        eventId
+      }
+    }
+  }
+`; // Replace with GraphQL Query
+
+router.post("/commonPoaps", async (req: Request, res: Response) => {
+    try {
+
+        const variables = {
+            lens1: req.body.lens1,
+            lens2: req.body.lens2
+        }
+        const { data, error } = await fetchQuery(query, variables);
+        const { user1, user2 } = data;
+        // check if there are poap that have matching eventid between user1 and user2
+        const user1Poaps = user1.poaps;
+        const user2Poaps = user2.poaps;
+        const user1PoapEventIds = user1Poaps.map((poap: { eventId: string }) => poap.eventId);
+        const user2PoapEventIds = user2Poaps.map((poap: { eventId: string }) => poap.eventId);
+        const commonPoapEventIds = user1PoapEventIds.filter((eventId: string) => user2PoapEventIds.includes(eventId));
+        res.status(200).json({
+            commonPoapEventIds: commonPoapEventIds
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+});
+
+export default router;  
