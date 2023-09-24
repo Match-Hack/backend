a0a11c847c21e406078ea4f6aa9fc85c29adab8a
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-09-24 03:10:36 +0200

    add a filter to get ride of profile already liked

--- src/routes/profileFiltered.ts
+++ src/routes/profileFiltered.ts
@@ -1,6 +1,6 @@
 import { gql } from 'graphql-request';
 import { Client, cacheExchange, fetchExchange } from '@urql/core';
-
+import like from '../models/like';
 
 import { init, fetchQuery } from "@airstack/node";
 import express, {
@@ -16,18 +16,18 @@ router.use(json());
 import dotenv from 'dotenv';
 dotenv.config();
 const LENS_API_URL = 'https://api.lens.dev';
-const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY ?? "";
-init(AIRSTACK_API_KEY);
 
-const urqlClient= new Client(
+
+
+const urqlClient = new Client(
     {
-    url: LENS_API_URL,
-    exchanges: [cacheExchange, fetchExchange],
-  }
-  )
+        url: LENS_API_URL,
+        exchanges: [cacheExchange, fetchExchange],
+    }
+)
 
-  const pingQuery = gql`query Profiles($lensArray : [Handle!]) {
-    profiles(request: { handles: $lensArray, limit: 5 }) {
+const pingQuery = gql`query Profiles($lensArray : [Handle!]) {
+    profiles(request: { handles: $lensArray, limit: 20 }) {
       items {
         name
         bio
@@ -77,35 +77,47 @@ const urqlClient= new Client(
     }
   }`;
 // function to find all the lens profile that are in the same hackathon
-
-async function findLensProfileInSameHackathon(hackathonName: string, lenshandle: string) {
+const findLensProfileLiked = async (lenshandle: string) => {
     try {
-        console.log("lenshandle : ", lenshandle);
-      const users = await user.find({ hackathon: hackathonName, lensProfile: { $ne: lenshandle } });
+      const users = await like.find({ from: lenshandle });
       console.log(users); 
-      const profiles = users.map((user: { lensProfile: string }) => user.lensProfile);
+      const profiles = users.map((user: { to: string }) => user.to);
       return profiles;
     } catch (error) {
       console.log(error);
     }
   }
-
-  router.post("/profileFiltered", async (req: Request, res: Response) => {
+  const findLensProfileNotLikedInSameHackathon = async (hackathonName: string, lenshandle: string) => {
     try {
-      const hackathonName = req.body.hackathonName;
-      const lenshandle = req.body.lens;
-      const lensProfiles = await findLensProfileInSameHackathon(hackathonName, lenshandle);
-      const response = await urqlClient.query(pingQuery, {
-        lensArray: lensProfiles
-      }).toPromise();
-      const items = response.data.profiles.items;
-      console.log("items : ", items); 
-      res.status(200).json(items);
+      const likedProfiles = await findLensProfileLiked(lenshandle);
+      console.log("liked : ",likedProfiles);
+      const users = await user.find({
+        hackathon: hackathonName,
+        lensProfile: { $ne: lenshandle,$nin: likedProfiles }
+      });
+      console.log(users);
+      const profiles = users.map((user: { lensProfile: string }) => user.lensProfile);
+      return profiles;
     } catch (error) {
       console.log(error);
-      res.status(500).json({
-        error: error
-      });
     }
-  });
+  };
+
+router.post("/profileFiltered", async (req: Request, res: Response) => {
+    try {
+        const hackathonName = req.body.hackathonName;
+        const lenshandle = req.body.lens;
+        const lensProfiles = await findLensProfileNotLikedInSameHackathon(hackathonName, lenshandle);
+        const response = await urqlClient.query(pingQuery, {
+            lensArray: lensProfiles
+        }).toPromise();
+        const items = response.data.profiles.items;
+        res.status(200).json(items);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: error
+        });
+    }
+});
 export default router;
