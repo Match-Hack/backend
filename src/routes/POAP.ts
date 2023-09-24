1cdcab0453f31730c553975147a52824158ef8dc
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-09-24 05:54:48 +0200

    handle the case where 2 users doesn't have poap in common

--- src/routes/POAP.ts
+++ src/routes/POAP.ts
@@ -1,10 +1,10 @@
 import { gql } from 'graphql-request';
 import { init, fetchQuery } from "@airstack/node";
 import express, {
-    Request,
-    Response,
-    Router,
-    json
+  Request,
+  Response,
+  Router,
+  json
 } from "express";
 import dotenv from 'dotenv';
 
@@ -45,29 +45,35 @@ const POAPInfoQuery = gql`query MyQuery($eventIds: [String!]) {
 }`;
 
 router.post("/commonPoaps", async (req: Request, res: Response) => {
-    try {
+  try {
 
-        const variables = {
-            lens1: req.body.lens1,
-            lens2: req.body.lens2
-        }
-        const { data, error } = await fetchQuery(commonPOAPQuery, variables);
-        const { user1, user2 } = data;
-        // check if there are poap that have matching eventid between user1 and user2
-        const user1Poaps = user1.poaps;
-        const user2Poaps = user2.poaps;
-        const user1PoapEventIds = user1Poaps.map((poap: { eventId: string }) => poap.eventId);
-        const user2PoapEventIds = user2Poaps.map((poap: { eventId: string }) => poap.eventId);
-        const commonPoapEventIds = user1PoapEventIds.filter((eventId: string) => user2PoapEventIds.includes(eventId));
-        const poapInfo = await fetchQuery(POAPInfoQuery, { eventIds: commonPoapEventIds });
-       
-        res.status(200).json({
-            commonPoapEventIds: poapInfo.data.PoapEvents.PoapEvent
-        })
+    const variables = {
+      lens1: req.body.lens1,
+      lens2: req.body.lens2
     }
-    catch (error) {
-        console.log(error);
+    const { data, error } = await fetchQuery(commonPOAPQuery, variables);
+    const { user1, user2 } = data;
+    // check if there are poap that have matching eventid between user1 and user2
+    const user1Poaps = user1.poaps;
+    const user2Poaps = user2.poaps;
+    const user1PoapEventIds = user1Poaps.map((poap: { eventId: string }) => poap.eventId);
+    const user2PoapEventIds = user2Poaps.map((poap: { eventId: string }) => poap.eventId);
+    const commonPoapEventIds = user1PoapEventIds.filter((eventId: string) => user2PoapEventIds.includes(eventId));
+    const poapInfo = await fetchQuery(POAPInfoQuery, { eventIds: commonPoapEventIds });
+
+    if (poapInfo && poapInfo.data && poapInfo.data.PoapEvents && poapInfo.data.PoapEvents.PoapEvent) {
+      res.status(200).json({
+        commonPoapEventIds: poapInfo.data.PoapEvents.PoapEvent
+      });
+    } else {
+      res.status(200).json({
+        commonPoapEventIds: null
+      });
     }
+  }
+  catch (error) {
+    console.log(error);
+  }
 
 });
 
